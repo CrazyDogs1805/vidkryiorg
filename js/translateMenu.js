f8dcbf752765795ce838ef7a97aab850faeea826
Nikita <crazy.vcdogs@gmail.com>
2017-06-14 23:02:49 +0300

    resize

--- js/translateMenu.js
+++
@@ -1,5 +0,0 @@
-var footer = document.getElementsByClassName('nav_footer');
-
-footer.innerHTML = '<div class="nav_footer"><ul class="col-xs-4 col-md-3"><li class=""><a href="gospel.html">Євангеліє</a></li><li class=""><a href="evidence.html" >Докази</a></li><li class=""><a href="articles.html" >Статті</a></li><li class=""><a href="about.html" >Про проект</a></li><li><a href="questions.html">Запитання</a></li></ul><ul class="col-xs-4 col-md-3"><li><a href="https://www.facebook.com/ccx.ukraine">Facebook</a></li><li><a href="https://vk.com/ccx.ukraine">Vkontakte</a></li></ul><ul class="col-xs-12 col-md-3"><li class="copyright"><span>Дизайн і розробка -</span><a href="#">CHEESEBANANA</a></li></ul></div>'
-
-console.log(footer);
